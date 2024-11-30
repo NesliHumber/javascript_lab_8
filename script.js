893384d929011630b0fc9037e8463dbfb683704c
NesliUsta <neslihan_iu81@hotmail.com>
2024-11-29 23:00:48 -0500

    Adds part1

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("============= Part 1: Understanding and Creating Objects =============");
+console.log("");
+
+// Student object:
+const student = {
+    name: "Neslihan Ustaoglu",
+    age: 40,
+    enrolled: true,
+    courses: ["Math", "Javascript", "Artificial Intelligence"],
+    displayInfo: function() {
+        return `${this.name} is ${this.age} years old.`;
+    },
+    // Method to add a new course:
+    addCourse: function(course) {
+        this.courses.push(course);
+        console.log(`Added new course: ${course}`);
+    },
+    // Method to calculate total number of courses:
+    getTotalCourses: function() {
+        return this.courses.length;
+    }
+};
+
+// Use console.log to output information:
+console.log(`Name: ${student.name}, Age: ${student.age}`);
+console.log(student.displayInfo());
